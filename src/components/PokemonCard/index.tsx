d37d12e0edff5b5b14c2df4c2d2baaed95003604
Suphadet Vatjanajaroenrat <suphadet.b@gmail.com>
2023-07-25 14:08:34 +0700

    add color

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -20,10 +20,14 @@ function firstUpper(str: string) {
 
 export default function PokemonCard({ number, name, imgUrl }: Props) {
     return (
-        <div className='flex flex-col bg-white w-[150px] rounded-lg border-black drop-shadow-xl p-3'>
-            <div className='flex justify-end text-gray-500'>{`#${addLeadingZeros(number)}`}</div>
-            <div className='flex justify-center'><Image className='' alt='dummy' width={150} height={150} src={imgUrl} /></div>
-            <div className='flex justify-center text-lg mt-1'>{firstUpper(name)}</div>
+        <div className='flex flex-col bg-white w-[150px] rounded-lg border-black drop-shadow-xl'>
+            <div className='absolute bottom-0 bg-gray-100 w-[100%] h-[50%] rounded-lg z-[-1]'/>
+                <div className='p-3'>
+                    <div className='flex justify-end text-gray-500'>{`#${addLeadingZeros(number)}`}</div>
+                    <div className='flex justify-center'><Image className='' alt='dummy' width={150} height={150} src={imgUrl} /></div>
+                    <div className='flex justify-center text-lg mt-1'>{firstUpper(name)}</div>
+                </div>
+     
         </div>
     )
 }
